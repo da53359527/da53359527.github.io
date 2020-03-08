fefc391901fe2bc8e46dd4ad5c2b992d515db40e
MaxSherlock <sherlockmax226@gmail.com>
2020-03-08 13:43:35 +0800

    second version

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
 $(document).ready(function () {
-  $('.scratch-zone').height(config.prizeZone.height + "%")
-  $('.scratch-zone').width(config.prizeZone.width + "%")
-  $('.scratch-zone').css({
-    left: (100 - config.prizeZone.width) / 2 + "%",
-    top: (100 - config.prizeZone.height) / 2 + "%",
+  var container = $('.scratch-container')
+  var scratchZone = $('.scratch-zone')
+
+  container.width($(window).width())
+  container.height((container.width() / 9) * 16)
+
+  scratchZone.width(container.width() * (config.prizeZone.width / 100))
+  scratchZone.height(container.height() * (config.prizeZone.height / 100))
+  scratchZone.css({
+      left: (100 - config.prizeZone.width) / 2 + "%",
+      top: (100 - config.prizeZone.height) / 2 + "%",
   })
 
-  var scratchZoneHeight = $('.scratch-zone').height()
-  var scratchZoneWidth = $('.scratch-zone').width()
   var prizePool = []
 
-  var canvas = $('#scratch')
+  var canvas = $('.mask')
   var ctx = canvas[0].getContext('2d')
-  canvas.width(scratchZoneWidth)
-  canvas.height(scratchZoneHeight)
-  ctx.canvas.width = scratchZoneWidth
-  ctx.canvas.height = scratchZoneHeight
+  canvas.width(scratchZone.width())
+  canvas.height(scratchZone.height())
+  ctx.canvas.width = scratchZone.width()
+  ctx.canvas.height = scratchZone.height()
 
   var maskImg = new Image()
   maskImg.src = 'imgs/mask.png'
 
   maskImg.onload = function () {
-    ctx.drawImage(maskImg, 0, 0, maskImg.width, maskImg.height, 0, 0, scratchZoneWidth, scratchZoneHeight)
+      ctx.drawImage(maskImg, 0, 0, maskImg.width, maskImg.height, 0, 0, scratchZone.width(), scratchZone.height())
+      scratchZone.find(".prize").show()
   }
 
   var brushImg = new Image()
@@ -38,116 +43,102 @@ $(document).ready(function () {
   var isDrawing = false
   var lastPoint
 
-  function gerenatePrizePool(prizeList) {
-    var pool = []
-
-    prizeList.forEach((prize) => {
-      for (var i = 0; i < prize.chance; i++) {
-        pool.push(prize.img)
-      }
-    })
-
-    return pool
-  }
-
   function getRandPrize() {
-    var random = Math.floor(Math.random() * prizePool.length - 1)
+      var min = 0
+      var max = prizePool.length - 1
+      var random = Math.floor(Math.random()*(max-min+1))+min
 
-    return prizePool[random]
+      return prizePool[random]
   }
 
   function makePrizePool() {
-    $.each(config.prize, (index, item) => {
-      for (var i = 0; i < item.chance; i++) {
-        prizePool.push(item.img)
-      }
-    })
+      $.each(config.prize, (index, item) => {
+          for (var i = 0; i < item.chance; i++) {
+              prizePool.push(item.img)
+          }
+      })
   }
 
   function handleMouseDown(e) {
-    isDrawing = true
-    lastPoint = getMouse(e, canvas)
+      isDrawing = true
+      lastPoint = getMouse(e, canvas)
   }
 
   function handleMouseMove(e) {
-    if (!isDrawing) {
-      return
-    }
+      if (!isDrawing) {
+          return
+      }
 
-    e.preventDefault()
+      e.preventDefault()
 
-    var currentPoint = getMouse(e, canvas),
-      dist = distanceBetween(lastPoint, currentPoint),
-      angle = angleBetween(lastPoint, currentPoint),
-      x,
-      y
+      var currentPoint = getMouse(e, canvas),
+          dist = distanceBetween(lastPoint, currentPoint),
+          angle = angleBetween(lastPoint, currentPoint),
+          x,
+          y
 
-    for (var i = 0; i < dist; i++) {
-      x = lastPoint.x + Math.sin(angle) * i - 25
-      y = lastPoint.y + Math.cos(angle) * i - 25
-      ctx.globalCompositeOperation = 'destination-out'
-      ctx.drawImage(brushImg, x, y, scratchZoneWidth * 0.1, scratchZoneWidth * 0.1)
-    }
+      for (var i = 0; i < dist; i++) {
+          x = lastPoint.x + Math.sin(angle) * i - 25
+          y = lastPoint.y + Math.cos(angle) * i - 25
+          ctx.globalCompositeOperation = 'destination-out'
+          ctx.drawImage(brushImg, x, y, scratchZone.width() * 0.1, scratchZone.width() * 0.1)
+      }
 
-    lastPoint = currentPoint
-    handlePercentage(getFilledInPixels(32))
+      lastPoint = currentPoint
+      handlePercentage(getFilledInPixels(32))
   }
 
   function handleMouseUp(e) {
-    isDrawing = false
+      isDrawing = false
   }
 
   function getMouse(e, canvas) {
-    var parentOffset = canvas.parent().offset()
-    var relX = (e.pageX || e.touches[0].clientX) - parentOffset.left
-    var relY = (e.pageY || e.touches[0].clientY) - parentOffset.top
+      var parentOffset = canvas.parent().offset()
+      var relX = (e.pageX || e.touches[0].clientX) - parentOffset.left
+      var relY = (e.pageY || e.touches[0].clientY) - parentOffset.top
 
-    return { x: relX, y: relY }
+      return { x: relX, y: relY }
   }
 
   function distanceBetween(point1, point2) {
-    return Math.sqrt(Math.pow(point2.x - point1.x, 2) + Math.pow(point2.y - point1.y, 2))
+      return Math.sqrt(Math.pow(point2.x - point1.x, 2) + Math.pow(point2.y - point1.y, 2))
   }
 
   function angleBetween(point1, point2) {
-    return Math.atan2(point2.x - point1.x, point2.y - point1.y)
+      return Math.atan2(point2.x - point1.x, point2.y - point1.y)
   }
 
   function handlePercentage(filledInPixels) {
-    filledInPixels = filledInPixels || 0
-    if (filledInPixels > config.openPercent) {
-      canvas
-        .parent()
-        .children('#scratch')
-        .hide()
-    }
+      filledInPixels = filledInPixels || 0
+      if (filledInPixels > config.openPercent) {
+          canvas
+              .parent()
+              .children('.mask')
+              .hide()
+      }
   }
 
   // Only test every `stride` pixel. `stride`x faster,
   // but might lead to inaccuracy
   function getFilledInPixels(stride) {
-    if (!stride || stride < 1) {
-      stride = 1
-    }
-
-    var pixels = ctx.getImageData(0, 0, scratchZoneWidth, scratchZoneHeight),
-      pdata = pixels.data,
-      l = pdata.length,
-      total = l / stride,
-      count = 0
-
-    // Iterate over all pixels
-    for (var i = (count = 0); i < l; i += stride) {
-      if (parseInt(pdata[i]) === 0) {
-        count++
+      if (!stride || stride < 1) {
+          stride = 1
       }
-    }
 
-    return Math.round((count / total) * 100)
-  }
+      var pixels = ctx.getImageData(0, 0, scratchZone.width(), scratchZone.height()),
+          pdata = pixels.data,
+          l = pdata.length,
+          total = l / stride,
+          count = 0
 
-  function resizeWindow(){
+      // Iterate over all pixels
+      for (var i = (count = 0); i < l; i += stride) {
+          if (parseInt(pdata[i]) === 0) {
+              count++
+          }
+      }
 
+      return Math.round((count / total) * 100)
   }
 
   makePrizePool()
