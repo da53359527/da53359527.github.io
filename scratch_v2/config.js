20137734ae0c170934be574360e53a79b66f0342
MaxSherlock <sherlockmax226@gmail.com>
2020-03-15 12:39:43 +0800

    update version 2 for test

---
+++ scratch_v2/config.js
@@ -0,0 +1,28 @@
+var config = {
+  "openPercent": 75, // 刮開多少百分比後, 自動清除遮罩   出廠設定值: 50
+  "pensize": 20,  // 筆的大小 1 ~ 100  出廠設定值: 15
+  "buttonGap": 15,
+  // 獎品池  chance: 開出機率,  img: 獎品圖片名稱(含副檔名) (ex. prize.png)
+  "prize": [
+    {
+      "chance": 1,
+      "img": "no-prize.png"
+    },
+    {
+      "chance": 1,
+      "img": "prize002.jpg"
+    },
+    {
+      "chance": 1,
+      "img": "prize003.jpg"
+    },
+    {
+      "chance": 1,
+      "img": "prize001.jpg"
+    },
+    {
+      "chance": 96,
+      "img": "prize004.png"
+    }
+  ]
+}
---
+++ scratch_v2/main.js
@@ -0,0 +1,172 @@
+$(document).ready(function () {
+    var container = $('.scratch-container')
+    var scratchZone = $('.scratch-zone')
+    var btnGroup = $('.btn-group')
+    var sound = new Audio();
+    sound.src = "sound/background.mp3"
+    sound.loop = true
+
+    container.width($(window).width())
+    container.height((container.width() / 9) * 16)
+
+    scratchZone.width(container.width())
+    scratchZone.height(container.height())
+
+    var prizePool = []
+
+    var canvas = $('.mask')
+    var ctx = canvas[0].getContext('2d')
+    canvas.width(scratchZone.width())
+    canvas.height(scratchZone.height())
+    ctx.canvas.width = scratchZone.width()
+    ctx.canvas.height = scratchZone.height()
+
+    var maskImg = new Image()
+    maskImg.src = 'imgs/mask.png'
+
+    maskImg.onload = function () {
+        ctx.drawImage(maskImg, 0, 0, maskImg.width, maskImg.height, 0, 0, scratchZone.width(), scratchZone.height())
+        scratchZone.find(".prize").show()
+    }
+
+    var brushImg = new Image()
+    brushImg.src = 'imgs/brush.png'
+
+    canvas.on('mousedown', handleMouseDown)
+    canvas.on('mousemove', handleMouseMove)
+    canvas.on('mouseup', handleMouseUp)
+
+    canvas.on('touchstart', handleMouseDown)
+    canvas.on('touchmove', handleMouseMove)
+    canvas.on('touchend', handleMouseUp)
+
+    var isDrawing = false
+    var lastPoint
+
+    function getRandPrize() {
+        var min = 0
+        var max = prizePool.length - 1
+        var random = Math.floor(Math.random() * (max - min + 1)) + min
+
+        return prizePool[random]
+    }
+
+    function makePrizePool() {
+        $.each(config.prize, (index, item) => {
+            for (var i = 0; i < item.chance; i++) {
+                prizePool.push(item.img)
+            }
+        })
+    }
+
+    function handleMouseDown(e) {
+        isDrawing = true
+        lastPoint = getMouse(e, canvas)
+    }
+
+    function handleMouseMove(e) {
+        if (!isDrawing) {
+            return
+        }
+
+        e.preventDefault()
+
+        var currentPoint = getMouse(e, canvas)
+        var dist = distanceBetween(lastPoint, currentPoint)
+        var angle = angleBetween(lastPoint, currentPoint)
+        var x
+        var y
+        var penSize = scratchZone.width() * (config.pensize / 100)
+
+
+
+        for (var i = 0; i < dist; i++) {
+            x = lastPoint.x + Math.sin(angle) * i - 25
+            y = lastPoint.y + Math.cos(angle) * i - 25
+            ctx.globalCompositeOperation = 'destination-out'
+            ctx.drawImage(brushImg, x, y, penSize, penSize)
+        }
+
+        lastPoint = currentPoint
+        handlePercentage(getFilledInPixels(32))
+    }
+
+    function handleMouseUp(e) {
+        isDrawing = false
+    }
+
+    function getMouse(e, canvas) {
+        var parentOffset = canvas.parent().offset()
+        var relX = (e.pageX || e.touches[0].clientX) - parentOffset.left
+        var relY = (e.pageY || e.touches[0].clientY) - parentOffset.top
+
+        return { x: relX, y: relY }
+    }
+
+    function distanceBetween(point1, point2) {
+        return Math.sqrt(Math.pow(point2.x - point1.x, 2) + Math.pow(point2.y - point1.y, 2))
+    }
+
+    function angleBetween(point1, point2) {
+        return Math.atan2(point2.x - point1.x, point2.y - point1.y)
+    }
+
+    function handlePercentage(filledInPixels) {
+        filledInPixels = filledInPixels || 0
+        if (filledInPixels > config.openPercent) {
+            canvas
+                .parent()
+                .children('.mask')
+                .hide()
+        }
+    }
+
+    // Only test every `stride` pixel. `stride`x faster,
+    // but might lead to inaccuracy
+    function getFilledInPixels(stride) {
+        if (!stride || stride < 1) {
+            stride = 1
+        }
+
+        var pixels = ctx.getImageData(0, 0, scratchZone.width(), scratchZone.height()),
+            pdata = pixels.data,
+            l = pdata.length,
+            total = l / stride,
+            count = 0
+
+        // Iterate over all pixels
+        for (var i = (count = 0); i < l; i += stride) {
+            if (parseInt(pdata[i]) === 0) {
+                count++
+            }
+        }
+
+        return Math.round((count / total) * 100)
+    }
+
+    makePrizePool()
+    $(".prize").attr("src", "imgs/prize/" + getRandPrize())
+
+    $(".btn-group").on("click", ".refresh", function () {
+        location.reload()
+    })
+
+    $(".btn-group").on("click", ".mute", function () {
+        $(this).removeClass("mute").addClass("unmute").attr("src", "imgs/unmute.png")
+        sound.play()
+    })
+
+    $(".btn-group").on("click", ".unmute", function () {
+        $(this).removeClass("unmute").addClass("mute").attr("src", "imgs/mute.png")
+        sound.pause()
+    })
+
+    $('.btn-group img').css({
+        "margin-left": config.buttonGap / 2 + "px",
+        "margin-right": config.buttonGap / 2 + "px"
+    })
+
+    btnGroup.css({
+        "margin-left": ((btnGroup.width() / 2) * -1) + "px",
+    })
+})
